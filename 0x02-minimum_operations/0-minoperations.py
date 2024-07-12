8929b52157a3b10f0f8be0d20266c38d70233019
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2024-07-12 03:13:29 +0000

    Update 0-minoperations file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,25 +5,14 @@ Module for minimum operations problem.
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters.
+    Function minOperations
+    Returns an integer
     """
-    if not isinstance(n, int) or n <= 0:
-        return 0
-
-    operations = 0
-    divisor = 2
-
+    answer = 0
+    neba = 2
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
-
-if __name__ == "__main__":
-    n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+        while n % neba == 0:
+            result += neba
+            n /= neba
+        neba += 1
+    return answer
